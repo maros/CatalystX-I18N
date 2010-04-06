4879fad547b3ab9b046ada8f874aeda1c1c84bbd
Maros Kollar <maros@k-1.com>
2010-04-06 09:59:41 +0200

    Fix base i18n role

--- lib/CatalystX/I18N/Role/Base.pm
+++ lib/CatalystX/I18N/Role/Base.pm
@@ -21,36 +21,36 @@ has 'timezone' => (
 );
 
 has 'locale' => (
-    is      => 'rw', 
+    is      => 'rw',
     isa     => 'Locale',
-    default => 'en_US.UTF-8',
+    default => 'en_US',
     trigger => \&_set_locale,
     predicate   => 'has_locale'
 );
 
 has 'datetime_locale' => (
-    is      => 'ro', 
+    is      => 'rw',
     isa     => 'DateTimeLocale',
 );
 
 has 'datetime_format_date' => (
-    is      => 'rw', 
+    is      => 'rw',
     isa     => 'DateTime::Format::CLDR'
 );
 
 has 'datetime_format_datetime' => (
-    is      => 'rw', 
+    is      => 'rw',
     isa     => 'DateTime::Format::CLDR'
 );
 
 has 'numberformat' => (
-    is      => 'rw', 
+    is      => 'rw',
     isa     => 'Number::Format'
 );
 
 has 'l10nhandle' => (
-    is      => 'rw', 
-    isa     => 'L10NHandle'
+    is      => 'rw',
+    #isa     => 'L10NHandle'
 );
 
 =head2 ACCESSORS
@@ -134,6 +134,12 @@ sub today {
     return $c->now->truncate( to => 'day' );
 }
 
+sub locale_config {
+    my ($c) = @_;
+    
+    return $c->config->{$c->locale} || {};
+}
+
 =head3 geodcode
  
  my $lgt = $c->geodcode
@@ -185,7 +191,7 @@ sub _set_locale {
     my ($c,$value) = @_;
     
     return 
-        unless $value =~ /^(?<language>[a-z]{2})_(?<territory>[A-Z]{2})(\..+)/;
+        unless $value =~ /^(?<language>[a-z]{2})_(?<territory>[A-Z]{2})(\..+)?/;
         
     my $language = lc($+{language});
     my $territory = uc($+{territory});
@@ -194,56 +200,57 @@ sub _set_locale {
     return 
         unless exists $c->config->{I18N}{locales}{$locale};
     
+    my $config = $c->config->{I18N}{locales}{$locale};
+    
     # Set posix locale
     setlocale( LC_CTYPE, $locale.'.UTF-8' );
     my $lconv = POSIX::localeconv();
     
     # Set number format
-    $c->numberformat(
-        new Number::Format(
-            -int_curr_symbol    => ($lconv->{int_curr_symbol} // 'EUR'),
-            -currency_symbol    => ($lconv->{currency_symbol} // '€'),
-            -mon_decimal_point  => ($lconv->{mon_decimal_point} // '.'),
-            -mon_thousands_sep  => ($lconv->{mon_thousands_sep} // ','),
-            -mon_grouping       => $lconv->{mon_grouping},
-            -positive_sign      => ($lconv->{positive_sign} // ''),
-            -negative_sign      => ($lconv->{negative_sign} // '-'),
-            -int_frac_digits    => ($lconv->{int_frac_digits} // 2),
-            -frac_digits        => ($lconv->{frac_digits} // 2),
-            -p_cs_precedes      => ($lconv->{p_cs_precedes} // 1),
-            -p_sep_by_space     => ($lconv->{p_sep_by_space} // 1),
-            -n_cs_precedes      => ($lconv->{n_cs_precedes} // 1),
-            -n_sep_by_space     => ($lconv->{n_sep_by_space} // 1),
-            -p_sign_posn        => ($lconv->{p_sign_posn} // 1),
-            -n_sign_posn        => ($lconv->{n_sign_posn} // 1),
+    my $numberformat = new Number::Format(
+        -int_curr_symbol    => ($config->{int_curr_symbol} // $lconv->{int_curr_symbol} // 'EUR'),
+        -currency_symbol    => ($config->{currency_symbol} // $lconv->{currency_symbol} // '€'),
+        -mon_decimal_point  => ($config->{mon_decimal_point} // $lconv->{mon_decimal_point} // '.'),
+        -mon_thousands_sep  => ($config->{mon_thousands_sep} // $lconv->{mon_thousands_sep} // ','),
+        -mon_grouping       => ($config->{mon_grouping} // $lconv->{mon_grouping}),
+        -positive_sign      => ($config->{positive_sign} // $lconv->{positive_sign} // ''),
+        -negative_sign      => ($config->{negative_sign} // $lconv->{negative_sign} // '-'),
+        -int_frac_digits    => ($config->{int_frac_digits} // $lconv->{int_frac_digits} // 2),
+        -frac_digits        => ($config->{frac_digits} // $lconv->{frac_digits} // 2),
+        -p_cs_precedes      => ($config->{p_cs_precedes} // $lconv->{p_cs_precedes} // 1),
+        -p_sep_by_space     => ($config->{p_sep_by_space} // $lconv->{p_sep_by_space} // 1),
+        -n_cs_precedes      => ($config->{n_cs_precedes} // $lconv->{n_cs_precedes} // 1),
+        -n_sep_by_space     => ($config->{n_sep_by_space} // $lconv->{n_sep_by_space} // 1),
+        -p_sign_posn        => ($config->{p_sign_posn} // $lconv->{p_sign_posn} // 1),
+        -n_sign_posn        => ($config->{n_sign_posn} // $lconv->{n_sign_posn} // 1),
 
-            -thousands_sep      => ($lconv->{thousands_sep} // ','),
-            -decimal_point      => ($lconv->{decimal_point} // '.'),
-            -grouping           => $lconv->{grouping},
-            
-            -decimal_fill       => 0,
-            -neg_format         => ($lconv->{negative_sign} // '-').'x',
-            -decimal_digits     => ($lconv->{frac_digits} // 2),
-        )
+        -thousands_sep      => ($config->{thousands_sep} // $lconv->{thousands_sep} // ','),
+        -decimal_point      => ($config->{decimal_point} // $lconv->{decimal_point} // '.'),
+#        -grouping           => ($config->{grouping} // $lconv->{grouping}),
+        
+        -decimal_fill       => ($config->{decimal_fill} // 0),
+        -neg_format         => ($config->{negative_sign} // $lconv->{negative_sign} // '-').'x',
+        -decimal_digits     => ($config->{frac_digits} // $lconv->{frac_digits} // 2),
     );
+    
+    $c->numberformat($numberformat);
 
     # Set datetime locale
     my $datetime_locale = DateTime::Locale->load( $locale );
     $c->datetime_locale($datetime_locale);
     
     # Set timezone
-    my $timezone = DateTime::TimeZone->new( name => $c->config->{I18N}{locales}{$locale}{timezone} )
-        || DateTime::TimeZone->new( name => 'UTC' );
+    my $timezone = DateTime::TimeZone->new( name => $config->{timezone} || 'Floating' );
     $c->timezone($timezone);
 
     # Set datetime_format_date
     my $datetime_format_date =
-        $c->config->{I18N}{locales}{$locale}{datetime_format_date} ||
+        $config->{datetime_format_date} ||
         $datetime_locale->date_format_medium;
         
     # Set datetime_format_datetime
     my $datetime_format_datetime =
-        $c->config->{I18N}{locales}{$locale}{datetime_format_datetime} ||
+        $config->{datetime_format_datetime} ||
         $datetime_locale->date_format_medium.' '.$datetime_locale->time_format_short;
     
     $c->datetime_format_date(
@@ -270,9 +277,14 @@ sub _set_locale {
     $c->session->{i18n_locale} = $locale
         if ($c->can('session'));
     
+    
+    my $l10nhandle = $c->model('L10N');
+    
+    warn('CAN maketext:'.$l10nhandle->can('maketext'));
+    
     # L10N Handle
     $c->l10nhandle(
-        $c->model('Model::L10N')
+        $l10nhandle
     );
 }
 

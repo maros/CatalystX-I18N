2eb7d27affbf93d4cd6edad22774e7e08100af90
Maros Kollar <maros@k-1.com>
2010-09-29 20:19:47 +0200

    Improve GetLocale code + add documentation

--- lib/CatalystX/I18N/Role/GetLocale.pm
+++ lib/CatalystX/I18N/Role/GetLocale.pm
@@ -13,8 +13,7 @@ sub check_locale {
         unless defined $locale
         && $locale =~ /^(?<language>[a-zA-Z]{2})_(?<territory>[a-zA-Z]{2})/;
     
-    my $language = lc($+{language});
-    my $territory = uc($+{territory});
+    $locale = lc($+{language}).'_'.uc($+{territory});
     
     return 
         if not exists $c->config->{I18N}{locales}{$locale}
@@ -50,27 +49,34 @@ sub get_locale_from_browser  {
     
     my ($languages,$territory);
     
-    # Get language and country/territory from browser
+    # Get Accept-Language
     if ($c->request->can('accept_languages')) {
-        $languages = $c->request->accept_languages;
-    }
-    
-    if ($c->request->can('browser_language')) {
-        my $language = $c->request->browser_language;
-        if (defined $languages
-            && ref $languages eq 'ARRAY') {
-            unshift(@$languages,$language)
-                unless grep { $language eq $_ } @$languages
-        } elsif ($language) {
-            $languages = [ $language ];
+        $languages = [ $c->request->accept_languages ];
+        # Check if Accept-Language matches a locale
+        foreach my $locale (@$languages) {
+            return $locale
+                if $c->check_locale($locale);
         }
+        # Strip territory/variant part
+        $languages = [ map { s/_[A-Za-z]{2}//; lc($_) } @$languages ];
     }
     
-    if ($c->request->can('browser_territory')) {
-        $territory = $c->request->browser_territory;
+    $languages ||= [];
+    
+    # Get browser language
+    if ($c->request->can('browser_language')) {
+        my $language = lc($c->request->browser_language);
+        unshift(@$languages,$language)
+            unless grep { $language eq $_ } @$languages
     }
+    
+    # Get client country
     if ($c->request->can('client_country')) {
-        $territory ||= $c->request->client_country;
+        $territory = uc($c->request->client_country);
+    }
+    # Get browser territory
+    if ($c->request->can('browser_territory')) {
+        $territory ||= uc($c->request->browser_territory);
     }
     
     my $locale_config = $c->config->{I18N}{locales};
@@ -92,7 +98,6 @@ sub get_locale_from_browser  {
         }
     }
     
-
     # Guess locale from language
     if (defined $languages) {
         foreach my $language (@$languages) {
@@ -130,4 +135,87 @@ sub get_locale {
     return $locale;
 }
 
+no Moose::Role;
 1;
+
+=head1 NAME
+
+CatalystX::I18N::Role::GetLocale - Tries to determine the current users locale
+
+=head1 SYNOPSIS
+
+ package MyApp::Catalyst;
+ 
+ use CatalystX::RoleApplicator;
+ use Catalyst qw/MyPlugins 
+    CatalystX::I18N::Role::Base
+    CatalystX::I18N::Role::GetLocale/;
+ 
+ __PACKAGE__->apply_request_class_roles(qw/CatalystX::I18N::Role::Request/);
+ 
+ package MyApp::Catalyst::Controller::Main;
+ use strict;
+ use warnings;
+ use parent qw/Catalyst::Controller/;
+ 
+ sub auto : Privat { # Auto method will always be called first
+     my ($self,$c) = @_;
+     $c->get_locale();
+ }
+=head1 DESCRIPTION
+
+This role provides many methods to retrieve/guess the best locale for the
+current user.
+
+=head1 MEDTHODS
+
+=head3 get_locale
+
+Tries to determine the users locale in the given order
+
+=over
+
+=item # Session (via C<get_locale_from_session>)
+
+=item # User (via C<get_locale_from_user>)
+
+=item # Browser (via C<get_locale_from_browser>)
+
+=item # Default locale from config (via C<$c-E<gt>config-E<gt>{I18N}{default_locale}>)
+
+=item # Random locale
+
+=back
+
+Sets the winning locale (via C<$c-E<gt>locale()>) if the 
+L<CatalystX::I18N::Role::Base> is loaded.
+
+=head3 get_locale_from_browser
+
+Tries to fetch the locale from the user object (via 
+L<$c-E<gt>user-E<gt>locale>)
+
+
+=head3 get_locale_from_session
+
+Tries to fetch the locale from the current session.
+
+=head3 get_locale_from_user
+
+Tries to fetch the locale from the user object (via 
+L<$c-E<gt>user-E<gt>locale>)
+
+=head3 check_locale
+
+=head1 SEE ALSO
+
+L<Locale::Maketext>, L<CatalystX::I18N::Model::L10N> 
+and L<CatalystX::I18N::L10N>
+
+=head1 AUTHOR
+
+    Maroš Kollár
+    CPAN ID: MAROS
+    maros [at] k-1.com
+    
+    L<http://www.revdev.at>

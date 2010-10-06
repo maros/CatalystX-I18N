279e21ff4d7778e38c54cabb44a203057b1ada11
Maros Kollar <maros@k-1.com>
2010-10-06 15:24:55 +0200

    Update model helper

--- lib/Catalyst/Helper/Model/L10N.pm
+++ lib/Catalyst/Helper/Model/L10N.pm
@@ -8,45 +8,6 @@ use warnings;
 use Path::Class;
 use FindBin;
 
-=head1 NAME
-
-Catalyst::Helper::Model::L10N - Helper for L10N models
-
-=head1 SYNOPSIS
-
-    script/myapp_create.pl model L10N L10N
-
-=head1 DESCRIPTION
-
-Helper for the L<Catalyst> L10N model.
-
-=head1 USAGE
-
-When creating a new L10N model class using this helper, you can
-specify much of the configuration and have it filled automatically.
-Using the example from the L</SYNOPSIS> section:
-
-=over
-
-=item * C<L10N>
-
-The name of the model.  This is also used to determine the filename,
-e.g. C<lib/MyApp/Model/L10N.pm>.
-
-=item * C<L10N>
-
-The helper to use, i.e. this one.
-
-=back
-
-=head1 METHODS
-
-=head2 mk_compclass
-
-Makes the L10N model class.
-
-=cut
-
 sub mk_compclass {
     my ($self, $helper) = @_;
 
@@ -59,25 +20,40 @@ sub mk_compclass {
     my $file = pop @path;
     
     my $l10ndir = $basedir->subdir( join '/', @path );
-    my $l10nfile = $l10ndir->file($file);
+    my $l10nfile = $l10ndir->file($file.'pm');
     $l10ndir->mkpath();
     
     $helper->render_file('l10nclass', $l10nfile->stringify, \%args);
     $helper->render_file('modelclass', $helper->{file}, \%args);
+    
     return 1;
 }
-
-=head2 mk_comptest
-
-Makes tests for the L10N model.
-
-=cut
 
 sub mk_comptest {
     my ($self, $helper) = @_;
 
     $helper->render_file('modeltest', $helper->{test});
 }
+
+=head1 NAME
+
+Catalyst::Helper::Model::L10N - Helper for L10N models
+
+=head1 SYNOPSIS
+
+    script/myapp_create.pl model L10N L10N
+
+=head1 DESCRIPTION
+
+Helper for the L<Catalyst> L10N model.
+
+=head1 ARGUMENTS
+
+   ./script/myapp_create.pl model <model_name> L10N
+
+You need to sepecify the C<model_name> (the name of the model).
+
+=cut
 
 =head1 SEE ALSO
 

898265605081dbdcee6b238d941d7d214f22adbb
Maros Kollar <maros@k-1.com>
2010-10-24 16:29:40 +0200

    Use JSON instead of JSON::Any

--- t/testapp/lib/TestApp/View/Test.pm
+++ t/testapp/lib/TestApp/View/Test.pm
@@ -4,13 +4,13 @@ use strict;
 use warnings;
 
 use parent 'Catalyst::View';
-use JSON::Any;
+use JSON;
 
 sub process {
     my ( $self, $c, $response) = @_;
 
     $c->response->content_type('application/json; charset=utf-8');
-    $c->response->body(JSON::Any->objToJson($response || $c->stash));
+    $c->response->body(encode_json($response || $c->stash));
     
     return;
 }

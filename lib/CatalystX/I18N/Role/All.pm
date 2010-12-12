49186aff7c42cab991f9f3acd2c32052d7a58139
Maros Kollar <maros@k-1.com>
2010-12-12 14:10:37 +0100

    Apply ViewTT role if possible

--- lib/CatalystX/I18N/Role/All.pm
+++ lib/CatalystX/I18N/Role/All.pm
@@ -16,6 +16,26 @@ with qw(
     CatalystX::I18N::Role::Collate
 );
 
+
+around 'setup_component' => sub {
+    my $orig  = shift;
+    my ($self,$component) = @_;
+    
+    Class::MOP::load_class($component);
+    
+    if ($component->isa('Catalyst::View::TT')
+        && $component->can('meta')) {
+        my $component_meta = $component->meta;
+        unless ($component_meta->does_role('CatalystX::I18N::TraitFor::ViewTT')) {
+            if ($component_meta->is_mutable) {
+                Moose::Util::apply_all_roles($component_meta, 'CatalystX::I18N::TraitFor::ViewTT')
+            }
+        }
+    }
+    
+    return $self->$orig($component);
+};
+
 no Moose::Role;
 1;
 
